d188dfddc6b03d2bf2d31f202d7b3a125b5e1b61
protatodev <thaddonaghue@yahoo.com>
2018-08-08 21:43:57 -0700

    added player detail page functionality

--- src/fantasy.js
+++ src/fantasy.js
@@ -4,6 +4,27 @@ export class Fantasy {
 
   }
 
+  getPlayerDetails(playerID) {
+
+    return new Promise((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let url = `https://api.fantasydata.net/v3/nfl/stats/JSON/Player/${playerID}`;
+
+      request.onload = function() {
+        if(request.status == 200) {
+          resolve(request.response);
+        } else {
+          reject(request.statusText);
+        }
+      }
+
+      request.open("GET", url, true);
+      request.setRequestHeader("Ocp-Apim-Subscription-Key", process.env.API_KEY)
+      request.send();
+
+    });
+  }
+
   getPlayerStats(season, week, position, sort) {
 
     return new Promise((resolve, reject) => {
--- src/main.js
+++ src/main.js
@@ -4,24 +4,52 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import {Fantasy} from './fantasy.js';
 
+function fetchPlayer(playerID) {
+  let fantasyGame = new Fantasy();
+  let promise = fantasyGame.getPlayerDetails(playerID);
+
+  promise.then(player => {
+    let newPlayer = JSON.parse(player);
+    document.getElementById("playerPhoto").src=newPlayer.PhotoUrl;
+    document.getElementById("playerName").innerHTML = newPlayer.FirstName + " " + newPlayer.LastName;
+    document.getElementById("playerTeam").innerHTML = newPlayer.Team;
+    document.getElementById("playerPosition").innerHTML = newPlayer.Position;
+    document.getElementById("playerCollege").innerHTML = newPlayer.College;
+    document.getElementById("playerExperience").innerHTML = newPlayer.Experience;
+    document.getElementById("playerHeight").innerHTML = newPlayer.Height;
+    document.getElementById("playerWeight").innerHTML = newPlayer.Weight;
+    document.getElementById("playerBirthdate").innerHTML = newPlayer.BirthDate;
+
+
+
+  }, error => {
+    console.log(error);
+  });
+}
+
 function displayStats(year,week,position) {
   let fantasyGame = new Fantasy();
   let promise = fantasyGame.getPlayerStats(year, week, position, "FantasyPoints");
+  
   promise.then(stats => {
     let newStats = JSON.parse(stats);
 
     for(let i = 0; i < 10; i++) {
-      $("#statsTable").append(`<tr class="tableRow" id="${newStats[i].PlayerID}"><td><a href="#">${newStats[i].Name}</a></td>
+      $("#statsTable").append(`<tr class="tableRow">
+                                   <td class="nameClickable" id="${newStats[i].PlayerID}">${newStats[i].Name}</td>
                                    <td>${newStats[i].Team}</td>
                                    <td>${newStats[i].Position}</td>
                                    <td>${newStats[i].Touchdowns}</td>
                                    <td>${newStats[i].FantasyPoints}</td>
-                                   <td>${newStats[i].OpponentPositionRank}</td>
-                                   <td>${newStats[i].WindSpeed}</td>
-
-
                                    </tr>`);
     }
+
+    $(".nameClickable").each(function() {
+      $(this).click(function() {
+        fetchPlayer(this.id);
+      });
+    });
+
   }, error => {
     console.log(error);
   });
@@ -42,6 +70,7 @@ $(document).ready(function() {
     let week = $("#week").val();
     let position = $("#position").val();
     displayStats(season, week, position);
+
   });
 
 });
